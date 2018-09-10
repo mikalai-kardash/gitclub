91b4ed7786bdb291c25082b990ce6af7204d9f7a
Mikalai Kardash <mikalai.kardash@gmail.com>
2018-09-09 19:51:57 -0700

    Some modifications.

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,5 +1,5 @@
 export function getAuthorizationHeader() {
     const username = process.env.USER_NAME
     const token = process.env.AUTH_TOKEN
-    return Buffer.from(`${username}:${token}`).toString('base64');
+    return Buffer.from(`${username}:${token}`).toString('base64')
 } 
